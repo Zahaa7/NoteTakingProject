1152b45e3639004a795d0cd240666dce892ddeca
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2020-09-22 12:58:06 +0300

    Implemented viewMenu functionality.

--- src/main/java/ro/jademy/notetaking/NoteApp.java
+++ src/main/java/ro/jademy/notetaking/NoteApp.java
@@ -3,6 +3,7 @@ package ro.jademy.notetaking;
 import ro.jademy.notetaking.models.Category;
 import ro.jademy.notetaking.models.Note;
 import ro.jademy.notetaking.services.IOService;
+import ro.jademy.notetaking.services.SortingService;
 
 import java.io.File;
 import java.util.Arrays;
@@ -12,9 +13,10 @@ import java.util.Scanner;
 public class NoteApp {
 
     private static Scanner input = new Scanner(System.in);
-    private List<Note> noteList;
-    private File notesFile;
-    private IOService ioService = new IOService();
+    private final List<Note> noteList;
+    private final File notesFile;
+    private final IOService ioService = new IOService();
+    private final SortingService sortingService = new SortingService();
 
     public NoteApp(List<Note> noteList, File notesFile) {
         this.noteList = noteList;
@@ -31,20 +33,22 @@ public class NoteApp {
                     ioService.writeJson(noteList, notesFile);
                     break;
                 case 2: // View All Notes By
-                    displayViewMenu();
+                    viewMenu();
                     break;
-                case 3: // View Note Content
-                   displayNotes();
+                case 3: // Search a note By
+
                     break;
-                case 4: // Edit Note
+                case 4: // Display All Notes
+                    printNotes(noteList);
                     break;
-                case 5: // Delete Note
+                case 5: // Edit Note Content
                     break;
-                case 6: // Exit Note App
+                case 6: //  Delete Note
+                    break;
+                case 7: // Exit Note App
                     System.exit(0);
-                    break;
                 default: // For invalid inputs
-                    System.out.println("Invalid input. Please, choose between [1-6] only!");
+                    System.out.println("Invalid input. Please, choose between [1-7] only!");
             }
         } while (true);
     }
@@ -59,10 +63,11 @@ public class NoteApp {
         System.out.println("         +-------  MAIN MENU  -------+         ");
         System.out.println("         |   1. Create a New Note    |         ");
         System.out.println("         |   2. View All Notes By    |         ");
-        System.out.println("         |   3. View Note Content    |         ");
-        System.out.println("         |   4. Edit Note Content    |         ");
-        System.out.println("         |   5. Delete Note          |         ");
-        System.out.println("         |   6. Exit Note App        |         ");
+        System.out.println("         |   3. Search a note By     |         ");
+        System.out.println("         |   4. Display All Notes    |         ");
+        System.out.println("         |   5. Edit Note Content    |         ");
+        System.out.println("         |   6. Delete Note          |         ");
+        System.out.println("         |   7. Exit Note App        |         ");
         System.out.println("         +---------------------------+         ");
     }
 
@@ -77,8 +82,28 @@ public class NoteApp {
         System.out.println("         | 2. Creation Date (Oldest) |         ");
         System.out.println("         | 3. Edit Date (Newest)     |         ");
         System.out.println("         | 4. Edit Date (Oldest)     |         ");
-        System.out.println("         | 5. Return to Main Menu    |         ");
+        System.out.println("         | 5. Unfinished Notes       |         ");
+        System.out.println("         | 6. Favorite Notes         |         ");
+        System.out.println("         | 7. Return to Main Menu    |         ");
         System.out.println("         +---------------------------+         ");
+    }
+
+    private void displaySearchMenu() {
+        System.out.println();
+        System.out.println("+---------------------------------------------+");
+        System.out.println("|           NOTE TAKING APPLICATION           |");
+        System.out.println("+---------------------------------------------+");
+        System.out.println();
+        System.out.println("        +-------  SEARCH MENU  -------+        ");
+        System.out.println("        |    1. Title                 |        ");
+        System.out.println("        |    2. Hashtag               |        ");
+        System.out.println("        |    3. Keyword               |        ");
+        System.out.println("        |    4. Return to Main Menu   |        ");
+        System.out.println("        +-----------------------------+        ");
+    }
+
+    private void printNotes(List<Note> notes) {
+        noteList.forEach(System.out::println);
     }
 
     private void createNewNote() {
@@ -96,9 +121,33 @@ public class NoteApp {
                 false, false, Category.NO_CATEGORY, Arrays.asList(hashtags)));
     }
 
-    private void displayNotes() {
-        noteList.forEach(System.out::println);
+    private void viewMenu() {
+        displayViewMenu();
+        byte option = input.nextByte();
+        switch (option) {
+            case 1: // Creation Date (Newest)
+                printNotes(sortingService.newestNotesList(noteList));
+                break;
+            case 2: // Creation Date (Oldest)
+                printNotes(sortingService.oldestNotesList(noteList));
+                break;
+            case 3: // Edit Date (Newest)
+                printNotes(sortingService.newestEditedNotesList(noteList));
+                break;
+            case 4: // Edit Date (Oldest)
+                printNotes(sortingService.oldestEditedNotesList(noteList));
+                break;
+            case 5: // Unfinished Notes
+                printNotes(sortingService.unfinishedNotesList(noteList));
+                break;
+            case 6: // Favorite Notes
+                printNotes(sortingService.favoriteNotesList(noteList));
+                break;
+            case 7: // Return to Main Menu
+                initiateNoteApp();
+                break;
+            default: // For invalid inputs
+                System.out.println("Invalid input. Please, choose between [1-7] only!");
+        }
     }
-
-
 }
