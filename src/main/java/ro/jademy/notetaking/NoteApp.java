0daa320562ebddf1679cf009121caa39430e8b6a
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2020-09-22 15:56:01 +0300

    Implemented editMenu functionality.

--- src/main/java/ro/jademy/notetaking/NoteApp.java
+++ src/main/java/ro/jademy/notetaking/NoteApp.java
@@ -4,12 +4,8 @@ import ro.jademy.notetaking.models.Category;
 import ro.jademy.notetaking.models.Note;
 import ro.jademy.notetaking.services.IOService;
 import ro.jademy.notetaking.services.SortingService;
-
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class NoteApp {
@@ -28,6 +24,7 @@ public class NoteApp {
     public void initiateNoteApp() {
         do {
             displayMainMenu();
+            System.out.println("Enter an option:");
             byte option = input.nextByte();
             switch (option) {
                 case 1: // Create a New Note
@@ -44,6 +41,8 @@ public class NoteApp {
                     printNotes(noteList);
                     break;
                 case 5: // Edit Note Content
+                    editNote();
+                    ioService.writeJson(noteList, notesFile);
                     break;
                 case 6: //  Delete Note
                     break;
@@ -104,7 +103,21 @@ public class NoteApp {
         System.out.println("        +-----------------------------+        ");
     }
 
-    private void printNotes(List<Note> notes) {
+    private void displayEditMenu() {
+        System.out.println();
+        System.out.println("+---------------------------------------------+");
+        System.out.println("|           NOTE TAKING APPLICATION           |");
+        System.out.println("+---------------------------------------------+");
+        System.out.println();
+        System.out.println("        +-------  EDIT MENU  -------+        ");
+        System.out.println("        |  1. Title                 |        ");
+        System.out.println("        |  2. Body                  |        ");
+        System.out.println("        |  3. Hashtag/s             |        ");
+        System.out.println("        |  4. Return to Main Menu   |        ");
+        System.out.println("        +---------------------------+        ");
+    }
+
+    private void printNotes(List<Note> noteList) {
         noteList.forEach(System.out::println);
     }
 
@@ -127,6 +140,7 @@ public class NoteApp {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ View Menu Implementation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
     private void viewMenu() {
         displayViewMenu();
+        System.out.println("Enter an option:");
         byte option = input.nextByte();
         switch (option) {
             case 1: // Creation Date (Newest)
@@ -158,21 +172,28 @@ public class NoteApp {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Search Menu Implementation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
     private List<Note> searchMenu() {
         displaySearchMenu();
+        System.out.println("Enter an option:");
         byte option = input.nextByte();
         String keyword;
         switch (option) {
             case 1: // Search by note's title
                 System.out.println("Title:");
+                input.skip("\n");
                 keyword = input.nextLine();
-                return searchNoteByTitle(keyword);
+                printNotes(searchNoteByTitle(keyword));
+                break;
             case 2: // Search by note's hashtag/s
                 System.out.println("Hashtag:");
+                input.skip("\n");
                 keyword = input.nextLine();
-                return searchNoteByHashtag(keyword);
+                printNotes(searchNoteByHashtag(keyword));
+                break;
             case 3: // Search by a given keyword
                 System.out.println("Keyword:");
+                input.skip("\n");
                 keyword = input.nextLine();
-                return searchNoteByKeyword(keyword);
+                printNotes(searchNoteByKeyword(keyword));
+                break;
             case 4: // Return to Main Menu
                 initiateNoteApp();
                 break;
@@ -183,18 +204,19 @@ public class NoteApp {
     }
 
     private List<Note> searchNoteByTitle(String keyword) {
-        return noteList.stream()
-                .filter(note -> note.getTitle().contains(keyword)).distinct().collect(Collectors.toList());
+        return new ArrayList<>((noteList.stream().filter(note -> note.getTitle().toLowerCase()
+                .contains(keyword.toLowerCase()))
+                .collect(Collectors.toList())));
     }
 
     private List<Note> searchNoteByHashtag(String keyword) {
         List<Note> tempList = new ArrayList<>();
         for (Note note : noteList) {
             for (String hashtag : note.getHashtagList()) {
-                if (hashtag.equalsIgnoreCase(keyword)) {
-                    tempList.add(note);
-                } else {
-                    System.out.println("No Notes with those hashtags!");
+                if (hashtag.toLowerCase().contains(keyword.toLowerCase())) {
+                    if (!tempList.contains(note)) {
+                        tempList.add(note);
+                    }
                 }
             }
         }
@@ -202,7 +224,67 @@ public class NoteApp {
     }
 
     private List<Note> searchNoteByKeyword(String keyword) {
-        return noteList.stream()
-                .filter(note -> note.getBody().contains(keyword)).distinct().collect(Collectors.toList());
+        return noteList.stream().filter(note -> note.getBody().toLowerCase()
+                .contains(keyword.toLowerCase())).collect(Collectors.toList());
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Edit Menu Implementation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    private void editNote() {
+        Note currentNote = selectNote(Objects.requireNonNull(searchMenu()));
+        System.out.println("Enter an option:");
+        displayEditMenu();
+        byte option = input.nextByte();
+        String keyword;
+        switch (option) {
+            case 1: // Edit note's title
+                System.out.println("New Title:");
+                input.skip("\n");
+                keyword = input.nextLine();
+                editNoteTitle(currentNote, keyword);
+                break;
+            case 2: // Edit note's body
+                System.out.println("New Body:");
+                input.skip("\n");
+                keyword = input.nextLine();
+                editNoteBody(currentNote, keyword);
+                break;
+            case 3: // Edit note's hashtag List
+                System.out.println("New hashtag/s:");
+                input.skip("\n");
+                keyword = input.nextLine();
+                editHashtag(currentNote, keyword);
+                break;
+            case 4: // Return to Main Menu
+                initiateNoteApp();
+                break;
+            default: // For invalid inputs
+                System.out.println("Invalid input. Please, choose between [1-4] only!");
+        }
+    }
+
+    private Note selectNote(List<Note> notes) {
+        for (int i = 0; i < notes.size(); i++) {
+            System.out.println((i + 1) + notes.get(0).getTitle());
+        }
+        System.out.println("Select only one note:");
+        byte selectedNote = input.nextByte();
+        return notes.get(selectedNote - 1);
+    }
+
+    private void editNoteTitle(Note note, String title) {
+        note.setTitle(title);
+        note.setModificationDate(System.currentTimeMillis());
+    }
+
+    private void editNoteBody(Note note, String body) {
+        note.setBody(body);
+        note.setModificationDate(System.currentTimeMillis());
+    }
+
+    private void editHashtag(Note note, String hashtag) {
+        List<String> hashtags = new ArrayList<>(Arrays.asList(hashtag.split("#")));
+        note.setHashtagList(hashtags);
+        note.setModificationDate(System.currentTimeMillis());
     }
 }
